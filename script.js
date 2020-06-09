0a30e40d12066acd8e595cb87d1512cdf826ee8b
Gabriel Hazi <hazigabriel@gmail.com>
2020-06-09 05:17:50 +0300

    coded removal buttons

--- script.js
+++ script.js
@@ -1,75 +1,58 @@
 let digits = document.querySelectorAll(".digit");
 let operators = document.querySelectorAll(".operator");
+let removalButtons = document.querySelectorAll(".removalButton")
 let resultValue = document.querySelector(".result");
 let currentOperation = document.querySelector(".currentOperation");
-let clearAll = document.querySelector(".clearAll");
-let backspace = document.querySelector(".backspace");
 
-let operation = {
+const operation = {
 	add: (a, b)  => Math.round( (a + b) * 100  ) / 100,
 	substract: (a, b) => Math.round( (a - b) * 100  ) / 100,
 	multiply: (a,b) => Math.round( (a * b) * 100  ) / 100,
 	divide: (a,b) => Math.round( (a / b) * 100  ) / 100,
 }
+const calculator = {
+	allClear: function(){
+		resultValue.innerHTML = "";
+		currentOperation.innerHTML = "";
+	} , 
+	backspace: function(){
+		const newStr = currentOperation.innerHTML.slice(0, -1); 
+		currentOperation.innerHTML = newStr;
 
+	} ,
+	appendNumber: function(number){
+ 
+		if( currentOperation.innerHTML ==  "" && number == ".") {
+			currentOperation.innerHTML = "0."
+			
+		} else if(number == "." && currentOperation.innerHTML.includes(".")) {
+			return
+		} else {
+			currentOperation.innerHTML += number;
+		}
+		//currentOperation.innerHTML += number;
+	} ,
+ 
+}
 
 // resultValue.addEventListener("click",function(){
 // 	resultValue.innerHTML = "asdsa"
 // 	currentOperation.innerHTML = "1234"
 // })
-
-// digits.forEach(function(digit){
-// 	digit.addEventListener("click", function(){
-// 		currentOperation.innerHTML +=  digit.querySelector("p").innerHTML
-// 	})
-// });
-
-// operators.forEach(function(operator){
-// 	operator.addEventListener("click", function(){
-// 		let currentOperator = operator.querySelector("p").innerHTML;
-// 		if(currentOperator == "/") {
-// 			resultValue.innerHTML = divide	(parseInt(resultValue.innerHTML), parseInt(currentOperation.innerHTML));
-// 			currentOperation.innerHTML = ""
-// 		} else if(currentOperator == "x"){
-// 			if(currentOperation.innerHTML == "") {
-// 				resultValue.innerHTML += currentOperator;
-// 			} else {
-// 				resultValue.innerHTML = multiply(parseInt(resultValue.innerHTML), parseInt(currentOperation.innerHTML));
-// 				currentOperation.innerHTML = ""
-// 			}
-		
-// 		} else if(currentOperator == "+") {
-// 			resultValue.innerHTML = add(parseInt(resultValue.innerHTML), parseInt(currentOperation.innerHTML));
-// 			currentOperation.innerHTML = ""
-		 
-// 		} else if(currentOperator == "-") {
-// 			resultValue.innerHTML = substract(parseInt(resultValue.innerHTML), parseInt(currentOperation.innerHTML));
-// 			currentOperation.innerHTML = ""
-// 		} else if(currentOperator == "."){
-// 			alert(" punct")
-// 		} else {
-// 			alert(" egal")
-// 		}
-// 	})
-// })
+removalButtons.forEach(function(e){
+	e.addEventListener("click", function(){
+		if(e.querySelector("p").innerHTML == "AC") {
+			calculator.allClear()
+		} else {
+			calculator.backspace()
+		}
+	})
+})
+digits.forEach(function(digit){
+	digit.addEventListener("click", function(){
+ 		calculator.appendNumber(digit.querySelector("p").innerHTML)
+ 	})
+});
 
 
-// function add(a,b) {
-// 	return a+b;
-// }
-// function substract(a, b) {
-// 	return a-b
-// //	toAdd.reduce((total,currentDigit) =>  {return total + currentDigit}, 0)
-// }
-// function multiply(a,b) {
-// 	return a * b 
-// }
-// function divide(a,b) {
-// 	return a / b 
-// }
-// let operate = function(operator, a, b) {
-	 
 
-
-// }
-//  
