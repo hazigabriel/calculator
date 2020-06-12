9cf85f362c83000286f16ffbdbec8ad095897da4
Gabriel Hazi <hazigabriel@gmail.com>
2020-06-12 23:17:46 +0300

    started adding code for displaying operation results

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let digits = document.querySelectorAll(".digit");
 let operators = document.querySelectorAll(".operator");
 let removalButtons = document.querySelectorAll(".removalButton")
 let resultValue = document.querySelector(".result");
-let currentOperation = document.querySelector(".currentOperation");
+let currentOperationValue = document.querySelector(".currentOperationValue");
+let currentOperatorDisplayed = document.querySelector(".currentOperatorDisplayed");
 
-const operation = {
+resultValue.innerHTML = ""
+const operate = {
 	add: (a, b)  => Math.round( (a + b) * 100  ) / 100,
 	substract: (a, b) => Math.round( (a - b) * 100  ) / 100,
 	multiply: (a,b) => Math.round( (a * b) * 100  ) / 100,
@@ -13,32 +15,36 @@ const operation = {
 const calculator = {
 	allClear: function(){
 		resultValue.innerHTML = "";
-		currentOperation.innerHTML = "";
+		currentOperationValue.innerHTML = "";
+		currentOperatorDisplayed.innerHTML = ""
 	} , 
 	backspace: function(){
-		const newStr = currentOperation.innerHTML.slice(0, -1); 
-		currentOperation.innerHTML = newStr;
+		if(currentOperationValue.innerHTML == "")  {
+			const newStr = resultValue.innerHTML.slice(0, -1); 
+			resultValue.innerHTML = newStr;
+		} else {
+			const newStr = currentOperationValue.innerHTML.slice(0, -1); 
+			currentOperationValue.innerHTML = newStr;
+		}
 
 	} ,
 	appendNumber: function(number){
  
-		if( currentOperation.innerHTML ==  "" && number == ".") {
-			currentOperation.innerHTML = "0."
+		if( currentOperationValue.innerHTML ==  "" && number == ".") {
+			currentOperationValue.innerHTML = "0."
 			
-		} else if(number == "." && currentOperation.innerHTML.includes(".")) {
-			return
-		} else {
-			currentOperation.innerHTML += number;
+		} else if(number == "." && currentOperationValue.innerHTML.includes(".")) {
+			return //detects if there is a dot within the current number, and stops the function
+		} else if(currentOperatorDisplayed.innerHTML == "") {
+			resultValue.innerHTML += number
+		} else { 
+			currentOperationValue.innerHTML += number;
 		}
-		//currentOperation.innerHTML += number;
+		
 	} ,
+
  
 }
-
-// resultValue.addEventListener("click",function(){
-// 	resultValue.innerHTML = "asdsa"
-// 	currentOperation.innerHTML = "1234"
-// })
 removalButtons.forEach(function(e){
 	e.addEventListener("click", function(){
 		if(e.querySelector("p").innerHTML == "AC") {
@@ -50,9 +56,80 @@ removalButtons.forEach(function(e){
 })
 digits.forEach(function(digit){
 	digit.addEventListener("click", function(){
- 		calculator.appendNumber(digit.querySelector("p").innerHTML)
+
+		calculator.appendNumber(digit.querySelector("p").innerHTML)
+
  	})
 });
+operators.forEach(function(currentOp){
+	currentOp.addEventListener("click", function(){
+
+		
+		let chosenOperator = currentOp.querySelector("p").innerHTML;
+		switch(currentOperatorDisplayed.innerHTML) {
+			case "/": 
+				if(currentOperationValue.innerHTML !== ""){
+					resultValue.innerHTML = operate.divide(resultValue.innerHTML, currentOperationValue.innerHTML);
+					currentOperatorDisplayed.innerHTML = chosenOperator;
+					currentOperationValue.innerHTML = ""
+					
+				} else {
+					currentOperatorDisplayed.innerHTML = chosenOperator
+				}
+				break;
+			case "x":
+				if(currentOperationValue.innerHTML !== ""){
+					resultValue.innerHTML = operate.multiply(resultValue.innerHTML, currentOperationValue.innerHTML);
+					currentOperatorDisplayed.innerHTML = chosenOperator;
+					currentOperationValue.innerHTML = ""
+				} else {
+					currentOperatorDisplayed.innerHTML = chosenOperator
+				}
+				break;
+			case "+":
+				if(currentOperationValue.innerHTML !== ""){
+					resultValue.innerHTML = operate.add(parseInt(resultValue.innerHTML), parseInt(currentOperationValue.innerHTML));
+					currentOperatorDisplayed.innerHTML = chosenOperator
+					currentOperationValue.innerHTML = "";
+				} else {
+					currentOperatorDisplayed.innerHTML = chosenOperator
+				}
+				break;
+			case "-":
+				if(currentOperationValue.innerHTML !== ""){
+					resultValue.innerHTML = operate.substract(resultValue.innerHTML, currentOperationValue.innerHTML);
+					currentOperatorDisplayed.innerHTML = chosenOperator;
+					currentOperationValue.innerHTML = ""
+				} else {
+					currentOperatorDisplayed.innerHTML = chosenOperator
+				}
+				break;
+			default: 
+				break
+		}
+
+		switch(chosenOperator) {
+			case "/": 
+				currentOperatorDisplayed.innerHTML = chosenOperator;
+				break;
+			case "x":
+				currentOperatorDisplayed.innerHTML = chosenOperator;
+				break;
+			case "+":
+				currentOperatorDisplayed.innerHTML = chosenOperator;
+				break;
+			case "-":
+				currentOperatorDisplayed.innerHTML = chosenOperator;
+				break;
+			default: 
+				break
+		}
 
 
 
+	})
+})
+
+
+//resultValue.innerText = parseInt(currentOperationValue.innerText) + parseInt(resultValue.innerText) 
+//resultValue.innerText = parseInt(resultValue.innerText) / parseInt(currentOperationValue.innerText)
