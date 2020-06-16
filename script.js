66a7371a3046439ea9aad94e4ef01176ddd15ddc
Gabriel Hazi <hazigabriel@gmail.com>
2020-06-16 03:19:00 +0300

    fixed 2 bugs, where after an op was assigned we can still backspace digits from result, and for where an operator was displayed if we did not had any digits inputed

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ let removalButtons = document.querySelectorAll(".removalButton")
 let resultValue = document.querySelector(".result");
 let currentOperationValue = document.querySelector(".currentOperationValue");
 let currentOperatorDisplayed = document.querySelector(".currentOperatorDisplayed");
+let shouldWeDel = 1; //with the backspace function, if we have values assigned to both
+//currentOperation & resultOperation, and we remove all digits from the currentOperation
+//the function starts removing digits from the result value, which should not be modified
+//if it's the result of an operation. using this toggle, after an operator is assigned to result we are unable to delete digits
 
 const operate = {
-	add: (a, b)  => Math.round( (a + b) * 100  ) / 100, 
+	add: (a, b)  => Math.round( (a + b) * 100  ) / 100 , 
 	substract: (a, b) => Math.round( (a - b) * 100  ) / 100,
 	multiply: (a,b) => Math.round( (a * b) * 100  ) / 100,
 	divide: (a,b) => Math.round( (a / b) * 100  ) / 100, // by running this ecuation on our function
@@ -16,12 +20,17 @@ const calculator = {
 	allClear: function(){
 		resultValue.innerHTML = "";
 		currentOperationValue.innerHTML = "";
-		currentOperatorDisplayed.innerHTML = ""
+		currentOperatorDisplayed.innerHTML = "";
+		shouldWeDel = 1;
 	} , 
 	backspace: function(){
 		if(currentOperationValue.innerHTML == "")  {
-			const newStr = resultValue.innerHTML.slice(0, -1); 
-			resultValue.innerHTML = newStr;
+			if(shouldWeDel == 1) {
+				const newStr = resultValue.innerHTML.slice(0, -1); 
+				resultValue.innerHTML = newStr;
+			} else {
+				 return
+			}
 		} else {
 			const newStr = currentOperationValue.innerHTML.slice(0, -1); 
 			currentOperationValue.innerHTML = newStr;
@@ -31,8 +40,9 @@ const calculator = {
 	appendNumber: function(number){
 		// the following code handles where should the number and dot be assigned
         if(currentOperationValue.innerHTML == "" ){
+
         	if( resultValue.innerHTML ==  "" && number == ".") {
-			resultValue.innerHTML = "0."
+				resultValue.innerHTML = "0."
 			
 			} else if(number == "." && resultValue.innerHTML.includes(".") ) {
 				return //detects if there is a dot within the current number, and stops the function
@@ -42,7 +52,6 @@ const calculator = {
 				currentOperationValue.innerHTML += number;
 			}
         } else {
-
         	if( number == "." && currentOperationValue.innerHTML ==  "" ) {
 				currentOperationValue.innerHTML = "0."
 			
@@ -59,6 +68,7 @@ const calculator = {
 
  
 }
+calculator.allClear();
 removalButtons.forEach(function(e){
 	e.addEventListener("click", function(){
 		if(e.querySelector("p").innerHTML == "AC") {
@@ -82,65 +92,75 @@ operators.forEach(function(currentOp){
 
 		
 		let chosenOperator = currentOp.querySelector("p").innerHTML;
-		switch(currentOperatorDisplayed.innerHTML) {
-			case "/": 
-				if(currentOperationValue.innerHTML !== ""){
-					resultValue.innerHTML = operate.divide(resultValue.innerHTML, currentOperationValue.innerHTML);
-					currentOperationValue.innerHTML = ""
-					
-				} else {
-					currentOperatorDisplayed.innerHTML = chosenOperator
-				}
-				break;
-			case "x":
-				if(currentOperationValue.innerHTML !== ""){
-					resultValue.innerHTML = operate.multiply(resultValue.innerHTML, currentOperationValue.innerHTML); 
-					currentOperationValue.innerHTML = ""
-				} else {
-					currentOperatorDisplayed.innerHTML = chosenOperator
-				}
-				break;
-			case "+":
-				if(currentOperationValue.innerHTML !== ""){
-					resultValue.innerHTML = operate.add(parseInt(resultValue.innerHTML), parseInt(currentOperationValue.innerHTML)); 
-					currentOperationValue.innerHTML = "";
-				} else {
-					currentOperatorDisplayed.innerHTML = chosenOperator
-				}
-				break;
-			case "-":
-				if(currentOperationValue.innerHTML !== ""){
-					resultValue.innerHTML = operate.substract(resultValue.innerHTML, currentOperationValue.innerHTML);
-					currentOperationValue.innerHTML = ""
-				} else {
-					currentOperatorDisplayed.innerHTML = chosenOperator
-				}
-				break;
+		if(resultValue.innerHTML !=- "") {  //if we do not have any value inputed, we cannot assign and display an operator
+			switch(currentOperatorDisplayed.innerHTML) {
+				case "/": 
+					if(currentOperationValue.innerHTML !== ""){
+						resultValue.innerHTML = operate.divide(resultValue.innerHTML, currentOperationValue.innerHTML);
+						currentOperationValue.innerHTML = ""
+						
+					} else {
+						currentOperatorDisplayed.innerHTML = chosenOperator
+					}
+					break;
+				case "x":
+					if(currentOperationValue.innerHTML !== ""){
+						resultValue.innerHTML = operate.multiply(resultValue.innerHTML, currentOperationValue.innerHTML); 
+						currentOperationValue.innerHTML = ""
+					} else {
+						currentOperatorDisplayed.innerHTML = chosenOperator
+					}
+					break;
+				case "+":
+					if(currentOperationValue.innerHTML !== ""){
+						resultValue.innerHTML = operate.add(parseInt(resultValue.innerHTML), parseInt(currentOperationValue.innerHTML)); 
+						currentOperationValue.innerHTML = "";
+					} else {
+						currentOperatorDisplayed.innerHTML = chosenOperator
+					}
+					break;
+				case "-":
+					if(currentOperationValue.innerHTML !== ""){
+						resultValue.innerHTML = operate.substract(resultValue.innerHTML, currentOperationValue.innerHTML);
+						currentOperationValue.innerHTML = ""
+					} else {
+						currentOperatorDisplayed.innerHTML = chosenOperator
+					}
+					break;
 
-			default: 
-				break
+				default: 
+					break
+			}
+			switch(chosenOperator) {
+				case "/": 
+					shouldWeDel = 0;
+					currentOperatorDisplayed.innerHTML = chosenOperator;
+					break;
+				case "x":
+					shouldWeDel = 0;
+					currentOperatorDisplayed.innerHTML = chosenOperator;
+					break;
+				case "+":
+					shouldWeDel = 0;
+					currentOperatorDisplayed.innerHTML = chosenOperator;
+					break;
+				case "-":
+					shouldWeDel = 0;
+					currentOperatorDisplayed.innerHTML = chosenOperator;
+					break;
+				case "=":
+					currentOperatorDisplayed.innerHTML = ""
+				default: 
+					break
+			}
+		} else {
+			return
 		}
-
-		switch(chosenOperator) {
-			case "/": 
-				currentOperatorDisplayed.innerHTML = chosenOperator;
-				break;
-			case "x":
-				currentOperatorDisplayed.innerHTML = chosenOperator;
-				break;
-			case "+":
-				currentOperatorDisplayed.innerHTML = chosenOperator;
-				break;
-			case "-":
-				currentOperatorDisplayed.innerHTML = chosenOperator;
-				break;
-			default: 
-				break
-		}
-
-
-
 	})
 })
-
-
+///bugs to fix:
+// when pressing DOt, if we do not have any value assigned to currentOperationValue we should start the line with "0.", instead of just "."
+//
+//
+//
+ 
